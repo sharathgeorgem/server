02f6b2845fda1d9c563ce4738a142a5a24645e3c
Sharath George M <sharathgeorgem@gmail.com>
2018-10-16 20:16:28 +0530

    Fixed request string parser

--- server.js
+++ server.js
@@ -5,20 +5,18 @@ var server = net.createServer()
 server.on('connection', (socket) => {
   var remoteAddress = socket.remoteAddress + ':' + socket.remotePort
   console.log('Gentlemen! We have a new client at ' + remoteAddress)
-
   socket.on('data', (data) => {
     console.log('Data from client is ' + data)
+    console.log('The request object is ' + JSON.stringify(requestParser(data), null, '\t'))
     socket.write('Hello client. \r\n\r\n')
   })
   socket.on('end', () => {
     console.log('Connection closed')
   })
 })
-
 server.on('error', (err) => {
   console.log('Oh damn. An error ' + err)
 })
-
 server.listen(9000, () => {
   var serverAddress = server.address()
   console.log('The server that is I, listens on ' + serverAddress.port)
@@ -26,38 +24,34 @@ server.listen(9000, () => {
 
 function requestParser (requestString) {
   var request = {}
-  var lines = requestString.split(/\r?\n/)
-
-  var parsedRequestStringLine = parseRequestString(lines).shift()
+  var lines = requestString.toString().split(/\r?\n/)
+  var parsedRequestStringLine = parseRequestString(lines.shift())
   request['method'] = parsedRequestStringLine['method']
   request['uri'] = parsedRequestStringLine['uri']
-
+  request['protocol'] = parsedRequestStringLine['protocol']
   var headerLines = []
   while (lines.length) {
     let line = lines.shift()
-    if (line === ' ') break
+    if (line === '') break
     headerLines.push(line)
   }
-
   request['headers'] = parseHeaders(headerLines)
   request['body'] = lines.join('\r\n')
-
   return request
 }
 
 function parseRequestString (lines) {
-  var section = lines.split(' ')
-  var parsed = {}
-  parsed['method'] = section[0]
-  parsed['uri'] = section[1]
-  parsed['protocol'] = section[2]
-
-  return parsed
+  var section = lines.toString().split(' ')
+  var parsedLineObject = {}
+  parsedLineObject['method'] = section.shift()
+  parsedLineObject['uri'] = section.shift()
+  parsedLineObject['protocol'] = section.join()
+  return parsedLineObject
 }
 
 function parseHeaders (headerString) {
   var headers = {}
-  for (let line in headerString) {
+  for (let line of headerString) {
     let parts = line.split(':')
     let key = parts.shift()
     headers[key] = parts.join(':').trim()
