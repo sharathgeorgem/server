e86128647f68c3c9c91e77fb8ead35c8ec0eb2e1
Sharath George M <sharathgeorgem@gmail.com>
2018-10-16 06:16:33 +0530

    Added Request Parser

--- server.js
+++ server.js
@@ -23,3 +23,44 @@ server.listen(9000, () => {
   var serverAddress = server.address()
   console.log('The server that is I, listens on ' + serverAddress.port)
 })
+
+function requestParser (requestString) {
+  var request = {}
+  var lines = requestString.split(/\r?\n/)
+
+  var parsedRequestStringLine = parseRequestString(lines).shift()
+  request['method'] = parsedRequestStringLine['method']
+  request['uri'] = parsedRequestStringLine['uri']
+
+  var headerLines = []
+  while (lines.length) {
+    let line = lines.shift()
+    if (line === ' ') break
+    headerLines.push(line)
+  }
+
+  request['headers'] = parseHeaders(headerLines)
+  request['body'] = lines.join('\r\n')
+
+  return request
+}
+
+function parseRequestString (lines) {
+  var section = lines.split(' ')
+  var parsed = {}
+  parsed['method'] = section[0]
+  parsed['uri'] = section[1]
+  parsed['protocol'] = section[2]
+
+  return parsed
+}
+
+function parseHeaders (headerString) {
+  var headers = {}
+  for (let line in headerString) {
+    let parts = line.split(':')
+    let key = parts.shift()
+    headers[key] = parts.join(':').trim()
+  }
+  return headers
+}
